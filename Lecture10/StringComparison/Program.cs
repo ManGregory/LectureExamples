2383460b0a7725bd19509c7c03a93b5bdf2cf981
ManGregory <manaenkov.gregory@gmail.com>
2020-02-23 00:54:38 +0200

    Added template for lecture 10, added music player

---
+++ Lecture10/StringComparison/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace StringComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Метод string.Compare(str1, str2) для сравнения порядка строк в последовательности
+            // -1 str1 предшествует str2
+            // 0 str1 == str2
+            // 1 str1 следует после str2
+            string str1 = "Иваненко";
+            string str2 = "Петренко";
+            string str3 = "Буруненко";
+
+            // -1, Иваненко предшествует Петренко
+            Console.WriteLine("Сравнение {0} и {1} = {2}", str1, str2, string.Compare(str1, str2));
+            // 0 Иваненко равен Иваненко
+            Console.WriteLine("Сравнение {0} и {1} = {2}", str1, "Иваненко", string.Compare(str1, "Иваненко"));
+            // 1 Иваненко после Буруненко
+            Console.WriteLine("Сравнение {0} и {1} = {2}", str1, str3, string.Compare(str1, str3));
+            // 1 Петренко после Буруненко
+            Console.WriteLine("Сравнение {0} и {1} = {2}", str2, str3, string.Compare(str2, str3));
+
+            Console.WriteLine();
+            Console.WriteLine("Игнорирование регистра");
+            // игнорирование регистра
+            // сравнение с учетом регистра по умолчанию
+            Console.WriteLine("Сравнение без игнорирования: {0}", string.Compare(str1, "иваненко"));
+            // добавляем еще один параметр ignoreCase = true, чтобы сравнить без учета регистра
+            Console.WriteLine("Сравнение с игнорированием: {0}", string.Compare(str1, "иваненко", true));
+
+            Console.WriteLine();
+            Console.WriteLine("Перевод в нижний и верхний регистр");
+            // перевод в нижний регистр
+            Console.WriteLine(str1.ToLower());
+            // перевод в верхний регистр
+            Console.WriteLine(str1.ToUpper());
+
+            Console.WriteLine();
+            Console.WriteLine("Проверка равенства всегда с учетом регистра");
+            Console.WriteLine(str1.Equals("иваненко"));
+            Console.WriteLine(str1 == "иваненко");
+            Console.WriteLine(str1.ToLower() == "иваненко");
+
+            Console.WriteLine();
+            Console.WriteLine("Проверка начинается или заканчивается строка с определенных символов");
+            Console.WriteLine(str1.StartsWith("Иван"));
+            Console.WriteLine(str1.EndsWith("ко"));
+
+            // string.Split()
+            var words = "ab cd".Split(' ');
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MusicConverter/MusicConverter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MusicConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hertzTable = new Dictionary<string, int>()
+            {
+                { "C", 262 },
+                { "C#", 277 },
+                { "D", 294 },
+                { "D#", 311 },
+                { "E", 330 },
+                { "F", 349 },
+                { "F#", 367 },
+                { "G", 392 },
+                { "G#", 415 },
+                { "A", 440 },
+                { "A#", 466 },
+                { "H", 494 }
+            };
+            string music = "80;4C4;4D4;4E4;4F4;4G4;4A4;4H4;4C5;4D5;4E5;4F5;4G5;4A5;4H5;";
+
+            var musicParts = music.Split(";", StringSplitOptions.RemoveEmptyEntries);
+            int bpm = int.Parse(musicParts[0]);
+            for (int index = 1; index < musicParts.Length; index++)
+            {                
+                string musicPart = musicParts[index];
+                bool oneDigitDuration = musicPart.Length == 3;
+                int duration = int.Parse(oneDigitDuration ? musicPart[0].ToString() : musicPart.Substring(0, 2));
+                string notes = musicPart[oneDigitDuration ? 1 : 2].ToString();
+                int octave = int.Parse(oneDigitDuration ? musicPart[2].ToString() : musicPart[3].ToString());
+                int noteHertz = hertzTable[notes];
+
+                for (int counter = 0; counter < Math.Abs(4 - octave); counter++)
+                {
+                    if (octave > 4)
+                    {
+                        noteHertz *= 2;
+                    }
+                    else
+                    {
+                        noteHertz /= 2;
+                    }
+                }
+
+                int quarterMilliseconds = (int)(60.0 / bpm * 1000);
+                Console.Beep(noteHertz, (int) (quarterMilliseconds / (duration / 4.0)));
+            }
+        }
+    }
+}
