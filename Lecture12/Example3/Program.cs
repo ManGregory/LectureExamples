6c5c16c126c03f2ebd819fd5755468ef74362ba2
ManGregory <manaenkov.gregory@gmail.com>
2020-03-02 22:05:14 +0200

    Examples for lecture 12

---
+++ Lecture12/Example3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example3
+{
+    /*
+     Написать программу телефонную книгу. 
+     Пользователь вводит имя человека и телефон, пока не нажмет ESC. 
+     После ввода данных пользователь может найти номер телефона по имени человека
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
